1ab844ebfcc4d0f3e7a5ae23e50b8ab80999e5d6
Meet Guleria <meetguleria94@gmail.com>
2023-11-26 22:29:57 -0500

    scrape shopify apps initial data

--- src/scraper.py
+++ src/scraper.py
@@ -2,6 +2,15 @@ import requests
 from bs4 import BeautifulSoup
 import csv
 import os
+import logging
+import time
+import random
+
+logging.basicConfig(level=logging.INFO)
+
+def generate_search_url(base_url, keyword):
+    formatted_keywords = '+'.join(keyword.split())
+    return f"{base_url}?q={formatted_keywords}&programs%5B%5D=built_for_shopify"
 
 def fetch_category_page(url):
     try:
@@ -16,14 +25,40 @@ def parse_app_list(html):
     soup = BeautifulSoup(html, 'html.parser')
     apps = []
 
-    #Replace 
-
+    # Find all search result cards
+    for app_card in soup.find_all("div", {"data-app-card-wrap": ""}):
+        app_name_tag = app_card.find("a", href=True)
+        if app_name_tag:
+            name = app_name_tag.get_text(strip=True)
+            url = app_name_tag['href']
+            apps.append((name, url))
     return apps
 
 def save_to_csv(app_data, filename="scraped_data.csv"):
     file_exists = os.path.isfile(filename)
     with open(filename, 'a', newline='') as csvfile:
-        writer = csv.DictWriter(csvfile, fieldnames=['App Name', 'URL'])
+        writer = csv.writer(csvfile)
         if not file_exists:
             writer.writeheader()
-        writer.writerow({ 'App Name': app_data[0], 'URL': app_data[1] })
+        writer.writerow(app_data)
+
+def run_search():
+    for keyword in keywords:
+        search_url = generate_search_url(base_url, keyword)
+        category_page = fetch_category_page(search_url)
+        if category_page:
+            app_list = parse_app_list(category_page)
+            for app_data in app_list:
+                save_to_csv(app_data)
+
+        delay = random.uniform(1, 10)
+        time.sleep(delay)
+
+keywords = ["product recommendations", "product suggestions", "sales forecasting", 
+"market analysis"]
+
+if __name__ == "__main__":
+    base_url = "https://apps.shopify.com/search"
+    logging.info("Starting scraping process.")
+    run_search()
+    logging.info("Scraping process completed")
--- src/search_manager.py
+++ src/search_manager.py
@@ -1,25 +1,24 @@
 import time
 import random
 from scraper import fetch_all_pages, save_to_csv
+import logging
 
 def generate_search_url(base_url, keyword):
     formatted_keyword = '+'.join(keyword.split())
-    return f"{base_url}?q={formatted_keyword}&st_source=autocomplete&programs%5B%5D=built_for_shopify"
+    return f"{base_url}?q={formatted_keyword}&programs%5B%5D=built_for_shopify"
 
-def main():
+def run_search():
     base_url = "https://apps.shopify.com/search"
     keywords = ["product recommendations", "product suggestions", "sales forecasting", "market analysis"]
 
     scraped_apps = set()
     for keyword in keywords:
+        logging.info(f"Searching for keyword: {keyword}")
         search_url = generate_search_url(base_url, keyword)
         apps = fetch_all_pages(search_url)
         for app in apps:
             if app[1] not in scraped_apps:
                 save_to_csv(app)
                 scraped_apps.add(app[1])
-
+        logging.info(f"Completed scraping for keyword: ")
         time.sleep(random.uniform(5, 15))
-
-if __name__ == "__main__":
-    main()
