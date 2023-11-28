1821eb4cc9ff7a3676191fc39f8a2d4deb3d9793
Meet Guleria <meetguleria94@gmail.com>
2023-11-27 22:37:11 -0500

    chromium crashes issue

--- src/scraper.py
+++ src/scraper.py
@@ -1,5 +1,5 @@
-import requests
-from bs4 import BeautifulSoup
+import asyncio
+from pyppeteer import launch
 import csv
 import os
 import logging
@@ -8,57 +8,63 @@ import random
 
 logging.basicConfig(level=logging.INFO)
 
-def generate_search_url(base_url, keyword):
+base_url = "https://apps.shopify.com/search"
+
+def generate_search_url(keyword):
     formatted_keywords = '+'.join(keyword.split())
     return f"{base_url}?q={formatted_keywords}&programs%5B%5D=built_for_shopify"
 
-def fetch_category_page(url):
-    try:
-        response = requests.get(url)
-        response.raise_for_status()
-        return response.text
-    except requests.RequestException as e:
-        print(f"Request failed: {e}")
-        return None
-
-def parse_app_list(html):
-    soup = BeautifulSoup(html, 'html.parser')
-    apps = []
-
-    # Find all search result cards
-    for app_card in soup.find_all("div", {"data-app-card-wrap": ""}):
-        app_name_tag = app_card.find("a", href=True)
-        if app_name_tag:
-            name = app_name_tag.get_text(strip=True)
-            url = app_name_tag['href']
-            apps.append((name, url))
-    return apps
+async def scrape_page(page, url):
+    try: 
+        await page.goto(url, { 'waitUntil': 'networkidle0'})
+        await page.waitForSelector("#search_app_grid-content", {"timeout": 60000})
+        apps = await page.evaluate('''() => {
+            const apps = [];
+            const appElements = document.querySelectorAll('#search_app_grid-content div[data-app-card-wrap]');
+            for (const app of appElements) {
+                const nameElement = app.querySelector('div.tw-text-heading-6 a');
+                const name = nameElement ? nameElement.innerText: 'Unknown';
+                const url = nameElement ? nameElement.href : 'Unknown';
+                apps.push([name, url]);
+            }
+            return apps;
+        }''')
+        print("Sample extracted apps:", apps[:5])
+        return apps
+    except Exception as e:
+        logging.error(f"An error occurred while scraping {url}: {e}")
+#        content = await page.content()
+#        logging.error(f"Page Content: {content}")
+        return []
 
 def save_to_csv(app_data, filename="scraped_data.csv"):
     file_exists = os.path.isfile(filename)
     with open(filename, 'a', newline='') as csvfile:
         writer = csv.writer(csvfile)
         if not file_exists:
-            writer.writeheader()
-        writer.writerow(app_data)
+            writer.writerow(['App Name', 'URL'])
+        for app in app_data:
+            writer.writerow(app)
 
-def run_search():
-    for keyword in keywords:
-        search_url = generate_search_url(base_url, keyword)
-        category_page = fetch_category_page(search_url)
-        if category_page:
-            app_list = parse_app_list(category_page)
-            for app_data in app_list:
-                save_to_csv(app_data)
+async def run_search():
+    try:
+        browser = await launch(headless=False)
+        context = await browser.createIncognitoBrowserContext()
+        page = await context.newPage()
 
-        delay = random.uniform(1, 10)
-        time.sleep(delay)
+        for keyword in ["product recommendations", "product suggestions", "sales forecasting", "market analysis"]:
+            search_url = generate_search_url(keyword)
+            apps = await scrape_page(page, search_url)
+            save_to_csv(apps)
+        
+            delay = random.uniform(1, 10)
+            await asyncio.sleep(delay)
 
-keywords = ["product recommendations", "product suggestions", "sales forecasting", 
-"market analysis"]
+        await browser.close()
+    except Exception as e:
+        logging.error(f"An error occurred in run_search: {e}")
 
 if __name__ == "__main__":
-    base_url = "https://apps.shopify.com/search"
     logging.info("Starting scraping process.")
-    run_search()
+    asyncio.get_event_loop().run_until_complete(run_search())
     logging.info("Scraping process completed")
